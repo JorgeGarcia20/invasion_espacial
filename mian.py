60035aad8439617cfa60e5b15c8f62ba72e1af7d
Jorge Alberto Garcia Estrada <alberto-garcia20@outlook.com>
2022-05-26 22:08:33 -0500

    Agregando a los enemigos del juego y creando funciones de movimiento

--- mian.py
+++ mian.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Inicializar a pygame
 pygame.init()
@@ -14,15 +15,27 @@ pygame.display.set_icon(icono)
 # Jugador
 img_jugador = pygame.image.load('./img/rocket.png')
 jugador_x = 368
-jugador_y = 536
+jugador_y = 500
 jugador_x_cambio = 0
-jugador_y_cambio = 0
+
+# Enemigo
+img_enemigo = pygame.image.load('./img/enemigo.png')
+enemigo_x = random.randint(0, 736)
+enemigo_y = random.randint(50, 200)
+enemigo_x_cambio = 0.3
+enemigo_y_cambio = 50
 
 # Funcion jugador
 
 
 def jugador(x, y):
     pantalla.blit(img_jugador, (x, y))
+
+
+# Funcion enemigo
+
+def enemigo(x, y):
+    pantalla.blit(img_enemigo, (x, y))
 
 
 # Loop del juego
@@ -57,15 +70,28 @@ while se_ejecuta:
     # Modificar movimiento
     jugador_x += jugador_x_cambio
 
-    # Mantener dentro de bordes
+    # Mantener dentro de bordes al jugador
     if jugador_x <= 0:
         jugador_x = 0
 
     elif jugador_x >= 736:
         jugador_x = 736
 
+    # Modificar movimiento del enemigo
+    enemigo_x += enemigo_x_cambio
+
+    # Mantener dentro de bordes al enemigo
+    if enemigo_x <= 0:
+        enemigo_x_cambio = 0.3
+        enemigo_y += enemigo_y_cambio
+
+    elif enemigo_x >= 736:
+        enemigo_x_cambio = -0.3
+        enemigo_y += enemigo_y_cambio
+
     # Pintando al jugador
     jugador(jugador_x, jugador_y)
+    enemigo(enemigo_x, enemigo_y)
 
     # Actualizar pantalla
     pygame.display.update()
